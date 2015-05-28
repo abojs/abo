c57e6085a2473e261ae91988696c16ffdb279e6a
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-05-28 23:31:18 +0300

    assign-expt created.

---
+++ test/assign-expt.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var assignExpt = require('../lib/assign-expt');
+var cookie = require('../lib/cookie');
+var _ = require('lodash');
+var vkey = 'abo.x';
+
+describe('Assign experiment', function() {
+  it('from cookie', function() {
+    cookie.set(vkey, '1');
+    var expt = assignExpt({
+      '1': {
+        id: '1',
+        name: 'Foo'
+      }
+    });
+    expect(expt.id).toBe('1');
+  });
+
+  it('remove from cookie if experiment doesn`t exist and no new assignable experiments', function() {
+    cookie.set(vkey, '2');
+    var expt = assignExpt({
+      '1': {
+        id: '1',
+        name: 'Foo',
+        ac: _.constant(false)
+      }
+    });
+    expect(expt).toBe(null);
+    expect(cookie.get(vkey)).toBe(null);
+  });
+
+  it('add new experiment to the cookie', function() {
+    cookie.remove(vkey);
+    var expt = assignExpt({
+      '1': {
+        id: '1',
+        name: 'Foo'
+      }
+    });
+    expect(expt.id).toBe('1');
+    expect(cookie.get(vkey)).toBe('1');
+  });
+});
